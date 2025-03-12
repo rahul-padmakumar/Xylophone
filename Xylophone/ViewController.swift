eacbd9274f5f1758a9cdc1b89b5a58b35279ec3b
rahulp <rahulp@qburst.com>
2025-03-12 17:35:52 +0530

    Complete xylophone code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func onPressed(_ sender: UIButton) {
+        playSound(title: sender.currentTitle ?? "")
+        adjustAlpha(button: sender)
+    }
     
-
+    func playSound(title: String){
+        print("Pressed \(title)")
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
+    func adjustAlpha(button: UIButton){
+        animateAlpha {
+            button.alpha = 0.5
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+            self.animateAlpha {
+                button.alpha = 1
+            }
+        }
+    }
+    
+    func animateAlpha(f1:@escaping () -> Void){
+        UIView.animate(withDuration: 0.3){
+            f1()
+        }
+    }
+    
 }
 
